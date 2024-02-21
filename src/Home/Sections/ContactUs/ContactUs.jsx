0b306814114172b099da6d9b098753c531eb47bc
unicornverse <jigarr.kushwaha@gmail.com>
2024-02-21 19:39:28 +0530

    ghfh

--- src/Home/Sections/ContactUs/ContactUs.jsx
+++ src/Home/Sections/ContactUs/ContactUs.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import contactImg from "../../../assets/contactImg.png"
 
 const ContactUs = () => {
   return (
    <>
    <div className="contactUsSection">
-    
+        <h4> <span>Contact</span> Us</h4>
+
+        <div className="contactusDiv">
+            <div className="contactUsLeft">
+                    <img src={contactImg} alt="" />
+                    <p>Our communication hub awaits!
+Here, you&#39;ll find various channels to connect with us. Whether you have questions, feedback, or need assistance, we're just a message away. Explore our contact form and social media links to get in touch. We value your input and are here to assist you promptly!</p>
+            </div>
+            <div className="contactUsRight">
+
+            </div>
+        </div>
    </div>
    </>
   )
