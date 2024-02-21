5231facc0bc3a0d3dd52f8e2ae3a70a241ba4dff
jigar <jigarr.kushwaha@gmail.com>
2024-02-22 01:42:28 +0530

    accordion,pricing,contact

--- src/Home/Sections/ContactUs/ContactUs.jsx
+++ src/Home/Sections/ContactUs/ContactUs.jsx
@@ -1,25 +1,48 @@
-import React from 'react'
-import contactImg from "../../../assets/contactImg.png"
+import React from "react";
+import contactImg from "../../../assets/contactImg.png";
 
 const ContactUs = () => {
   return (
-   <>
-   <div className="contactUsSection">
-        <h4> <span>Contact</span> Us</h4>
+    <>
+    <div className="contactUsContainner">
+
+    
+      <div className="contactUsSection">
+        <h4>
+          <span>Contact</span> Us
+        </h4>
 
         <div className="contactusDiv">
-            <div className="contactUsLeft">
-                    <img src={contactImg} alt="" />
-                    <p>Our communication hub awaits!
-Here, you&#39;ll find various channels to connect with us. Whether you have questions, feedback, or need assistance, we're just a message away. Explore our contact form and social media links to get in touch. We value your input and are here to assist you promptly!</p>
-            </div>
-            <div className="contactUsRight">
+          <div className="contactUsLeft">
+            <img src={contactImg} alt="" />
+            <p>
+              Our communication hub awaits! Here, you&#39;ll find various
+              channels to connect with us. Whether you have questions, feedback,
+              or need assistance, we're just a message away. Explore our contact
+              form and social media links to get in touch. We value your input
+              and are here to assist you promptly!
+            </p>
+          </div>
+          <div className="contactUsRight">
 
+            <div className="contactForm">
+              <form action="">
+                <label htmlFor="email">Email</label>
+                <input type="email" />
+                <label htmlFor="topic">Topic Title</label>
+                <input type="text" />
+                <label htmlFor="message">Message</label>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+
+                <button>Submit</button>
+              </form>
             </div>
+          </div>
         </div>
-   </div>
-   </>
-  )
-}
+      </div>
+      </div>
+    </>
+  );
+};
 
-export default ContactUs
+export default ContactUs;
