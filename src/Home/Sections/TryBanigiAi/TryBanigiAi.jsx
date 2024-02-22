22a37fd0a7fbb4e858f5e966261865e524a86034
unicornverse <jigarr.kushwaha@gmail.com>
2024-02-22 19:33:59 +0530

    trySlider

--- src/Home/Sections/TryBanigiAi/TryBanigiAi.jsx
+++ src/Home/Sections/TryBanigiAi/TryBanigiAi.jsx
@@ -1,40 +1,87 @@
 import React, { useState } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import {  Pagination } from 'swiper/modules';
+import 'swiper/css/pagination';
+import 'swiper/css';
+
+
+import t1 from "../../../assets/t1.png";
+import t2 from "../../../assets/t2.png";
+import t3 from "../../../assets/t3.png";
+import t4 from "../../../assets/t4.png";
+
+
+
 import TryDesignForm from '../../../Components/TryDesignForm'
 
 const TryBanigiAi = () => {
 
-  const [activeTab,setActiveTab]=useState(0);
+  const [activeTab, setActiveTab] = useState(0);
   const tabs = [
-    { title: 'Tab 1', content: <TryDesignForm/> },
-    { title: 'Tab 2', content: 'Content 2' },
-    { title: 'Tab 3', content: 'Content 3' }
+    { title: 'Interior Design', content: <TryDesignForm /> },
+    { title: 'Exterior Design', content: 'Content 2' },
+    { title: 'Landscape Design', content: 'Content 3' },
+    { title: 'Custom Design', content: 'Content 4' }
   ];
   const handleTabClick = (index) => {
     setActiveTab(index);
   };
   const renderTabs = () => {
     return tabs.map((tab, index) => (
-      <div
+      <button
         key={index}
         onClick={() => handleTabClick(index)}
         className={activeTab === index ? 'active' : ''}
       >
         {tab.title}
-      </div>
+      </button>
     ));
   };
   return (
     <>
-    
-    <div className='TryDesignDiv'>
+
+      <div className='TryDesignDiv'>
         <h3>Try <span>Banigi AI </span>Design</h3>
 
-        
+
         <div className="tabs-container">
-      {renderTabs()}
-      <div className="tab-content">{tabs[activeTab]?.content}</div>
-    </div>
-    </div>
+          <div className="tab-heading">
+          {renderTabs()}
+          </div>
+
+          <div className="tab_content_div">
+          <div className="tab-content">{tabs[activeTab]?.content}</div>
+          
+
+          <Swiper
+                    modules={[ Pagination]}
+
+                    spaceBetween={50}
+                    slidesPerView={1}
+                    
+                    pagination={{
+                        clickable: true,
+                    }}
+
+                    className="mySwiper"
+
+                >
+                    <SwiperSlide><img src={t1} alt="" /></SwiperSlide>
+                    <SwiperSlide><img src={t2} alt="" /></SwiperSlide>
+                    <SwiperSlide><img src={t3} alt="" /></SwiperSlide>
+                    <SwiperSlide><img src={t4} alt="" /></SwiperSlide>
+                    
+
+                    
+
+                </Swiper>
+
+              
+          
+          </div>
+          
+        </div>
+      </div>
 
     </>
   )
