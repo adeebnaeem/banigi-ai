a6f3cf9d391f4ec900a1febc7d293415ac61b45c
unicornverse <jigarr.kushwaha@gmail.com>
2024-02-27 19:33:11 +0530

    adsdas

--- src/Components/PrivacyPolicy.jsx
+++ src/Components/PrivacyPolicy.jsx
@@ -5,22 +5,30 @@ const PrivacyPolicy = () => {
   return (
     <>
       <div className="legalModalContainer">
-        <div className="modalHeader">
-          <h3>Privacy and Policy</h3>
-          <img src={modalClose} alt="" />
-        </div>
-        <div className="modalText">
-          <p>At BanigiAI, protecting our users privacy is a top priority. We are committed to ensuring the confidentiality and security of your personal information. This privacy policy outlines our practices concerning the collection, use, and sharing of personal information through our website and mobile application.
-            Information We Collect
-          </p>
-          
-          <span>Use of Personal Information</span>
-          <p>We utilize the personal information we collect to maintain and enhance our website and services. We may also use it to send you promotional materials or updates.
-Sharing of Personal Information
-We may share your personal information with third parties, such as service providers, to facilitate our services or as required by law.
-</p>
-        </div>
+        <div className="legalModalOverlay">
+          <div className="legalModalDiv">
 
+
+            <div className="modalHeader">
+              <h3>Privacy and Policy</h3>
+              <img src={modalClose} alt="" />
+            </div>
+
+            <div className="modalText">
+              <p>At BanigiAI, protecting our users privacy is a top priority. We are committed to ensuring the confidentiality and security of your personal information. This privacy policy outlines our practices concerning the collection, use, and sharing of personal information through our website and mobile application.
+                Information We Collect
+              </p>
+
+              <span>Use of Personal Information</span>
+              <p>We utilize the personal information we collect to maintain and enhance our website and services. We may also use it to send you promotional materials or updates.
+                Sharing of Personal Information
+                We may share your personal information with third parties, such as service providers, to facilitate our services or as required by law.
+              </p>
+            </div>
+
+            
+          </div>
+        </div>
       </div>
 
     </>
