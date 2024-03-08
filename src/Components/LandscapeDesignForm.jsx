121745507585fc63ab8380cb8700766d5d168bca
jigar <jigarr.kushwaha@gmail.com>
2024-03-08 23:58:48 +0530

    home page design

--- src/Components/LandscapeDesignForm.jsx
+++ src/Components/LandscapeDesignForm.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
 import PrimaryButton from './PrimaryButton';
+import customStyles from "../Components/selectCustomStyle";
+import Select from "react-select";
+
+
 
 const LandscapeDesignForm = () => {
     const [selectedFile, setSelectedFile] = useState(null);
@@ -9,6 +13,56 @@ const LandscapeDesignForm = () => {
         setSelectedFile(file);
         setSelectedName(file.name);
     };
+
+    const landscapeOptions = [
+        { value: 'Modern', label: 'Modern' },
+        { value: 'City', label: 'City' },
+        { value: 'Contemporary', label: 'Contemporary' },
+        { value: 'Luxury', label: 'Luxury' },
+        { value: 'Apartment', label: 'Apartment' },
+        { value: 'Small', label: 'Small' },
+        { value: 'Vegetable', label: 'Vegetable' },
+        { value: 'Low Budget', label: 'Low Budget' },
+        { value: 'Beach', label: 'Beach' },
+        { value: 'Wedding', label: 'Wedding' },
+        { value: 'Rural Garden', label: 'Rural Garden' },
+        { value: 'Mediterranean', label: 'Mediterranean' },
+        { value: 'Restaurant Garden', label: 'Restaurant Garden' },
+        { value: 'Formal', label: 'Formal' },
+        { value: 'American', label: 'American' },
+        { value: 'English', label: 'English' },
+        { value: 'Traditional', label: 'Traditional' },
+        { value: 'Christmas', label: 'Christmas' },
+        { value: 'Meditation', label: 'Meditation' },
+        { value: 'Coastal', label: 'Coastal' },
+        { value: 'Tropical', label: 'Tropical' },
+      ];
+      const ModeOptions = [
+        { value: 'Beautiful Redesign', label: 'Beautiful Redesign' },
+        { value: 'Creative Redesign', label: 'Creative Redesign' },
+        { value: 'Fill The Room', label: 'Fill The Room' },
+      ];
+      const styleOptions = [
+        { value: 'Backyard', label: 'Backyard' },
+        { value: 'Patio', label: 'Patio' },
+        { value: 'Terrace', label: 'Terrace' },
+        { value: 'Front Yard', label: 'Front Yard' },
+        { value: 'Courtyard', label: 'Courtyard' },
+        { value: 'Pool Area', label: 'Pool Area' },
+        { value: 'Porch', label: 'Porch' },
+        { value: 'Playground', label: 'Playground' },
+      
+    
+      ];
+      const NumberOfDesignOptions = [
+        { value: '1', label: '1' },
+        { value: '2', label: '2' },
+        { value: '3', label: '3' },
+        { value: '4', label: '4' },
+        { value: '5', label: '5' },
+      ];
+
+
 
     return (
         <>
@@ -27,59 +81,42 @@ const LandscapeDesignForm = () => {
 
 
                         <label htmlFor="">Landscape Type</label>
-                        <select name="" id="" className='custom-select'>
-                            <option value="">Backyard</option>
-                            <option value="">Patio</option>
-                            <option value="">Terrace</option>
-                            <option value="">Front Yard</option>
-                            <option value="">Courtyard</option>
-                            <option value="">Pool Area</option>
-                            <option value="">Porch</option>
-                            <option value="">Playground</option>
-                            
-                        </select>
+                       <Select
+                className="react-select-container"
+                classNamePrefix="react-select"
+                defaultValue={landscapeOptions[0]}
+                options={landscapeOptions}
+                styles={customStyles}
+                isSearchable={false}
+              />
                         <label htmlFor="">Mode</label>
-                        <select name="" id="" className='custom-select'>
-                            <option value="">Beautiful Redesign</option>
-                            <option value="">Creative Redesign</option>
-                            <option value="">Fill The Room</option>
-                            
-                        </select>
+                        <Select
+                className="react-select-container"
+                classNamePrefix="react-select"
+                defaultValue={ModeOptions[0]}
+                options={ModeOptions}
+                styles={customStyles}
+                isSearchable={false}
+              />
                         <label htmlFor="">Style</label>
-                        <select name="" id="" className='custom-select'>
-                            <option value="">Modern </option>
-                            <option value="">City </option>
-                            <option value="">Contemporary </option>
-                            <option value="">Luxury </option>
-                            <option value="">Apartment</option>
-                            <option value="">Small </option>
-                            <option value="">Vegetable </option>
-                            <option value="">Low Budget </option>
-                            <option value="">Beach </option>
-                            <option value="">Wedding </option>
-                            <option value="">Rural Garden </option>
-                            <option value="">Mediterranean</option>
-                            <option value="">Restaurant Garden </option>
-                            <option value="">Formal</option>
-                            <option value="">American</option>
-                            <option value="">English</option>
-                            <option value="">Traditional</option>
-                            <option value="">Christmas</option>
-                            <option value="">Meditation</option>
-                            <option value="">Coastal</option>
-                            <option value="">Tropical</option>
-                           
-
-                        </select>
+                        <Select
+                className="react-select-container"
+                classNamePrefix="react-select"
+                defaultValue={styleOptions[0]}
+                options={styleOptions}
+                styles={customStyles}
+                isSearchable={false}
+              />
                        
                         <label htmlFor="">Number Of Designs</label>
-                        <select name="" id="" className='custom-select'>
-                            <option value="">1</option>
-                            <option value="">2</option>
-                            <option value="">3</option>
-                            <option value="">4</option>
-                            <option value="">5</option>
-                        </select>
+                        <Select
+                className="react-select-container"
+                classNamePrefix="react-select"
+                defaultValue={NumberOfDesignOptions[0]}
+                options={NumberOfDesignOptions}
+                styles={customStyles}
+                isSearchable={false}
+              />
                         <label htmlFor="">AI Intervention</label>
                         <div className="intervation_radio">
                             <div className="veryLowInter">
