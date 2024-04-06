17e4d7b80952896e90b2f9a7bee7c60401d2701f
unicornverse <jigarr.kushwaha@gmail.com>
2024-04-06 16:57:07 +0530

    build job card responsive fix anf hero text font sixe reduce

--- src/Home/Sections/buildJob/BuildJob.jsx
+++ src/Home/Sections/buildJob/BuildJob.jsx
@@ -34,37 +34,191 @@ const BuildJob = () => {
                   height="39.18"
                   rx="19.59"
                   stroke="#C79952"
-                  stroke-width="1.62"
+                  strokeWidth="1.62"
                 />
                 <path
                   d="M17.0635 23.6611L24.4272 16.2975"
                   stroke="#C79952"
-                  stroke-width="1.62"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="1.62"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 />
                 <path
                   d="M19.1257 16.2722H24.5034V21.6498"
                   stroke="#C79952"
-                  stroke-width="1.62"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="1.62"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 />
               </svg>
             </div>
           </div>
-          <div>
+          <div className="first_job_Card">
             <img src={job2} alt="" />
+            <div className="job_card_text">
+              <p>
+                Banigi AI will make exterior design through AI power technology.
+                Builder & Remodeler can make their work efficiently in given
+                time frame.
+              </p>
+              <svg
+                width="42"
+                height="41"
+                viewBox="0 0 42 41"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  x="1.4101"
+                  y="0.807315"
+                  width="39.18"
+                  height="39.18"
+                  rx="19.59"
+                  stroke="#C79952"
+                  strokeWidth="1.62"
+                />
+                <path
+                  d="M17.0635 23.6611L24.4272 16.2975"
+                  stroke="#C79952"
+                  strokeWidth="1.62"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M19.1257 16.2722H24.5034V21.6498"
+                  stroke="#C79952"
+                  strokeWidth="1.62"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </div>
           </div>
-          <div>
+          <div className="first_job_Card">
             <img src={job3} alt="" />
+            <div className="job_card_text">
+              <p>
+                Banigi AI will make exterior design through AI power technology.
+                Builder & Remodeler can make their work efficiently in given
+                time frame.
+              </p>
+              <svg
+                width="42"
+                height="41"
+                viewBox="0 0 42 41"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  x="1.4101"
+                  y="0.807315"
+                  width="39.18"
+                  height="39.18"
+                  rx="19.59"
+                  stroke="#C79952"
+                  strokeWidth="1.62"
+                />
+                <path
+                  d="M17.0635 23.6611L24.4272 16.2975"
+                  stroke="#C79952"
+                  strokeWidth="1.62"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M19.1257 16.2722H24.5034V21.6498"
+                  stroke="#C79952"
+                  strokeWidth="1.62"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </div>
           </div>
-          <div>
+          <div className="first_job_Card">
             <img src={job4} alt="" />
+            <div className="job_card_text">
+              <p>
+                Banigi AI will make exterior design through AI power technology.
+                Builder & Remodeler can make their work efficiently in given
+                time frame.
+              </p>
+              <svg
+                width="42"
+                height="41"
+                viewBox="0 0 42 41"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  x="1.4101"
+                  y="0.807315"
+                  width="39.18"
+                  height="39.18"
+                  rx="19.59"
+                  stroke="#C79952"
+                  strokeWidth="1.62"
+                />
+                <path
+                  d="M17.0635 23.6611L24.4272 16.2975"
+                  stroke="#C79952"
+                  strokeWidth="1.62"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M19.1257 16.2722H24.5034V21.6498"
+                  stroke="#C79952"
+                  strokeWidth="1.62"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </div>
           </div>
-          <div>
+          <div className="first_job_Card">
             <img src={job5} alt="" />
+            <div className="job_card_text">
+              <p>
+                Banigi AI will make exterior design through AI power technology.
+                Builder & Remodeler can make their work efficiently in given
+                time frame.
+              </p>
+              <svg
+                width="42"
+                height="41"
+                viewBox="0 0 42 41"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  x="1.4101"
+                  y="0.807315"
+                  width="39.18"
+                  height="39.18"
+                  rx="19.59"
+                  stroke="#C79952"
+                  strokeWidth="1.62"
+                />
+                <path
+                  d="M17.0635 23.6611L24.4272 16.2975"
+                  stroke="#C79952"
+                  strokeWidth="1.62"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M19.1257 16.2722H24.5034V21.6498"
+                  stroke="#C79952"
+                  strokeWidth="1.62"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </div>
           </div>
+        
+         
         </div>
       </div>
     </>
